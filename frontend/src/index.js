4b25e512d280ff997aa81f51158d10469bb795dd
joshuahuggins-prog <joshua.huggins@googlemail.com>
2026-01-02 16:27:33 +0000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@/index.css";
-import App from "@/App";
+import "./index.css";
+import App from "./App";
+
+// PWA service worker
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+
+// Register service worker (offline + installable)
+serviceWorkerRegistration.register();
