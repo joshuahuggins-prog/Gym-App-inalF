94ae04e8791eda4acac1835bb744d800e092c4a2
joshuahuggins-prog <joshua.huggins@googlemail.com>
2026-01-05 08:26:50 +0000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { initStorage } from "./utils/storage";
+
+initStorage();
+
 // PWA service worker
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
